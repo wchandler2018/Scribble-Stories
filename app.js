ef1f49ab9f0c1c41772a8dae7cafd64db427cbc8
William Chandler <williamchandler@Williams-MacBook-Air.local>
2019-01-12 15:05:25 -0500

    created auth

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+
+//passport config 
+require("./config/passport")(passport);
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send("it is working");
-})
+
+app.get("/", (req, res) => {
+    res.send("it Works");
+});
+
+//Load Routes
+const auth = require("./routes/auth");
+
+//Use Routes
+app.use("/auth", auth);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>{
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:"mongodb://wchandler2018:devine11@ds155614.mlab.com:55614/scribblestories",
+    googleClientID: "267863757143-rius1palv9ovks824sq4e8k1o646pifp.apps.googleusercontent.com",
+    googleClientSecret:"ms93h_hiuSIzi3iS6EPBKEqg"
+}
